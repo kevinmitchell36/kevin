df4055c74df8a469911f7efc06851b4ffd363d5e
Kevin <kevingm31@gmail.com>
2017-11-26 08:56:10 -0600

    Add resume page/css fixes

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const app = express();
 
-// set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// index page 
 app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
-// about page 
 app.get('/about', function(req, res) {
     res.render('pages/about');
 });
 
-app.set('view engine', 'ejs');
+app.get('/resume', function(req, res) {
+    res.render('pages/resume');
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000')
