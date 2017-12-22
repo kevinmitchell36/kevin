ceb9c5725918da95411ff49003332ad7afc7555e
Kevin <kevingm31@gmail.com>
2017-12-21 20:07:07 -0600

    More CSS fixes

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', function(req, res) {
-    res.render('pages/index');
+app.get("/", function(req, res) {
+  res.render("pages/index");
 });
 
-app.get('/about', function(req, res) {
-    res.render('pages/about');
+app.get("/about", function(req, res) {
+  res.render("pages/about");
 });
 
-app.get('/resume', function(req, res) {
-    res.render('pages/resume');
+app.get("/resume", function(req, res) {
+  res.render("pages/resume");
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000')
+app.get("/projects", function(req, res) {
+  res.render("pages/projects");
 });
 
-app.use(express.static(__dirname + '/public'))
+app.get("/videos", function(req, res) {
+  res.render("pages/videos");
+});
 
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000");
+});
+
+app.use(express.static(__dirname + "/public"));
